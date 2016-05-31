7b111022e06bcb873b975389328d871ef6571b4c
LEDUNOIS Simon <simon.ledunois@gmail.com>
2016-05-31 18:50:30 +0200

    Adding comments.getMediaComments()

---
+++ test/comments.js
@@ -0,0 +1,21 @@
+/**
+ * Created by SLedunois on 01/05/2016.
+ */
+
+/**
+ * Json Object containing the app params
+ * @type {{client_id: string, client_secret: string, redirect_uri: string}}
+ */
+var params = require('./params.json');
+
+var should = require('should');
+var inodesta = require('../lib/main').inodesta(params);
+
+describe('#comments.getMediaComments(accessToken, mediaId, callback)', function() {
+    it('Returns an object containing a list of recent comments on a media object.', function(done) {
+       inodesta.comments.getMediaComments(params.access_token, params.media_id, function(result) {
+           result.should.be.type('object').have.property('meta').have.property('code', params.returned_status_code);
+           done();
+       });
+    });
+});
